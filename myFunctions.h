cdc464ff949ef91ee1dd224f8626a2304f514a5c
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-10-30 12:53:12 -0600

    Create myFunctions.h

---
+++ myFunctions.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+void askOS() {
+    #ifdef _WIN32 //Ask for the OS
+        cout << endl << endl;
+        system("pause");
+    #endif
+    
+    return;
+}
