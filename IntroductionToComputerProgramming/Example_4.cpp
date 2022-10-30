9e49e0271ab2e9a1e7d67c4452739677230a40ae
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-10-30 13:18:34 -0600

    Update Example_4.cpp

--- IntroductionToComputerProgramming/Example_4.cpp
+++ IntroductionToComputerProgramming/Example_4.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 int main()
 {
-	char character = 'X';
-	int integer = character; // ASCII code is placed inside an int variable
-    
+    char character = 'X';
+    int integer = character; // ASCII code is placed inside an int variable
+
     /*cout is able to recognize the actual type of its element even when it is an effect of a 
     conversion (an phenomenon which occurs when data is subject to type change).*/
-	cout << character << " " << static_cast<int>(character);
-	cout << " " << integer << " " << static_cast<char>(integer);
+    cout << character << " " << static_cast<int>(character);
+    cout << " " << integer << " " << static_cast<char>(integer);
 
     askOS();
     return 0;
