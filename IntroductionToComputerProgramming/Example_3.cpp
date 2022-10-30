cf1df820f1a6d77b0b3db47c173edfc353177dab
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-10-30 13:18:00 -0600

    Update Example_3.cpp

--- IntroductionToComputerProgramming/Example_3.cpp
+++ IntroductionToComputerProgramming/Example_3.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main()
 {
-	char character = 'X', minus = '-';
-	float floating = 2.5;
+    char character = 'X', minus = '-';
+    float floating = 2.5;
     /*In general, output streams (including cout) are able to recognize the type of the printed value and act accordingly 
     i.e. they’ll use a proper form of data presentation for char and float values.*/
-	cout << character << minus << floating;
+    cout << character << minus << floating;
 
     askOS();
     return 0;
