664602c6f34a2e6f417c1f9be20a574399551bc3
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-10-31 22:17:09 -0600

    Create Example_12.cpp

---
+++ AccessingData/Example_12.cpp
@@ -0,0 +1,23 @@
+/* Inputting strings
+
+Inputting strings is a bit more complicated, due to the fact that the cin stream treats spaces 
+(to be precise, not only regular spaces but also all so-called white characters) as delimiters, 
+demarcating limits between data. */
+#include <iostream>
+#include <string>
+#include "../myFunctions.h"
+
+using namespace std;
+
+int main() 
+{
+    string line_of_types;
+
+    cout << "Input string: ";
+    cin >> line_of_types;
+    cout << "String: ";
+    cout << line_of_types;
+
+    askOS();
+    return 0;
+}
