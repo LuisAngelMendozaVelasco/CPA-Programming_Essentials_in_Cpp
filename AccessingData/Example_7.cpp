048bba5440076c9c10e775d7d59bee90db9a93ab
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-10-31 20:54:39 -0600

    Create Example_7.cpp

---
+++ AccessingData/Example_7.cpp
@@ -0,0 +1,27 @@
+/* Conversions – gains and losses
+
+Conversions from floating point types to integral types always cause a loss of accuracy. 
+There’s no escaping this.  We always lose the fractional part of a float number.*/
+#include <iostream>
+#include <iomanip>
+#include "../myFunctions.h"
+
+using namespace std;
+
+int main() 
+{
+    /* Also when the float is extremely large (or extremely small) we’ll also experience a loss of value. 
+    This applies to the values beyond the scope of the target integral type.*/
+    float f = 123.456;
+    float g = 1e100;
+    int i = f;
+    int j = g;
+    std::cout << std::fixed;
+    std::cout << std::setprecision(3);
+
+    cout << f << " -> " << i << endl;
+    cout << g << " -> " << j;
+
+    askOS();
+    return 0;
+}
