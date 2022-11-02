8bc09ff06bc9568613773b42ecdaf50ce8b59580
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-11-01 23:35:51 -0600

    Create Example_32.cpp

---
+++ AccessingData/Example_32.cpp
@@ -0,0 +1,16 @@
+/* Name spaces
+
+We’ve qualified all the ambiguous names with a prefix consisting of the home name space (std) and a special operator written as “::”.
+The operator’s official name is “scope resolution operator”. It has some more applications, not only qualifying names with their home name spaces; 
+hence its name refers to more general concepts.
+A qualifying act like this is an alternative (explicite) way of using the “using namespace” statement.*/
+#include <iostream>
+#include "../myFunctions.h"
+
+int main() 
+{
+    std::cout << "Play as time goes by" << std::endl;
+
+    askOS();
+    return 0; 
+}
