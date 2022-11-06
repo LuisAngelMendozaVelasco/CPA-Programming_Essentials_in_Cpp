7e1fef07a6e9f0f44f2849662186709cedcbcace
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-11-06 14:13:26 -0600

    Update Example_20.cpp

--- Inheritance/Example_20.cpp
+++ Inheritance/Example_20.cpp
@@ -13,79 +13,79 @@ using namespace std;
 
 class Pet {
 protected: 
-	string name;
+    string name;
 public:
-	Pet(string name) : name(name) {}
-	virtual void make_sound()
-	{ 
-		cout << name << " is silent :(" << endl; 
-	}
+    Pet(string name) : name(name) {}
+    virtual void make_sound()
+    { 
+        cout << name << " is silent :(" << endl; 
+    }
 };
 
 class Dog : public Pet {
 public:	
-	Dog(string name) : Pet(name) {}
-	void make_sound()
-	{ 
-		cout << name << " says: Woof!" << endl; \
-	}
+    Dog(string name) : Pet(name) {}
+    void make_sound()
+    { 
+        cout << name << " says: Woof!" << endl; \
+    }
 };
 
 class GermanShepherd : public Dog {
 public:	
-	GermanShepherd(string name) : Dog(name) {}
-	void make_sound()
-	{ 
-		cout << name << " says: Wuff!" << endl; 
-	}
-	void laufen()
-	{ 
-		cout << name << " runs (shepherd)!" << endl; 
-	}
+    GermanShepherd(string name) : Dog(name) {}
+    void make_sound()
+    { 
+        cout << name << " says: Wuff!" << endl; 
+    }
+    void laufen()
+    { 
+        cout << name << " runs (shepherd)!" << endl; 
+    }
 };
 
 class MastinEspanol : public Dog {
 public:	
-	MastinEspanol(string name) : Dog(name) {}
-	void make_sound()
-	{ 
-		cout << name << " says: Guau!" << endl; 
-	}
-	void correr()
-	{ 
-		cout << name << " runs (mastin)!" << endl; 
-	}
+    MastinEspanol(string name) : Dog(name) {}
+    void make_sound()
+    { 
+        cout << name << " says: Guau!" << endl; 
+    }
+    void correr()
+    { 
+        cout << name << " runs (mastin)!" << endl; 
+    }
 };
 
 /* We catch the error and do nothing. Thanks to that, our dogs are ready to return to their normal life.
 The error that arises during the unsuccessful casting is simply ignored, but in effect (paradoxically), the casting is safe now 
 and will cause no harm in the case of failure.*/
 void play_with_pet(Pet &pet) {
-	pet.make_sound();
-	try {
-		dynamic_cast<GermanShepherd &>(pet).laufen();
-	} 
-	catch(...) {}
-	try {
-		dynamic_cast<MastinEspanol &>(pet).correr();
-	}
-	catch(...) {}
+    pet.make_sound();
+    try {
+        dynamic_cast<GermanShepherd &>(pet).laufen();
+    } 
+    catch(...) {}
+    try {
+        dynamic_cast<MastinEspanol &>(pet).correr();
+    }
+    catch(...) {}
 }
 
 int main() 
 {
-	Pet pet("creature");
-	Dog dog("Dog");
-	GermanShepherd shepherd("Hund");
-	MastinEspanol mastin("Perro");
-	play_with_pet(pet);
+    Pet pet("creature");
+    Dog dog("Dog");
+    GermanShepherd shepherd("Hund");
+    MastinEspanol mastin("Perro");
+    play_with_pet(pet);
     cout << endl;
-	play_with_pet(dog);
+    play_with_pet(dog);
     cout << endl;
-	play_with_pet(shepherd);
+    play_with_pet(shepherd);
     cout << endl;
-	play_with_pet(mastin);
+    play_with_pet(mastin);
 
     askOS();
     return 0; 
-}
+    }
