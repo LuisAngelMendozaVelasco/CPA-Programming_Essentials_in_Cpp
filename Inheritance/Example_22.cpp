3a7c191256750c482cbb2cbb087794e90b3f9ef0
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-11-06 20:46:00 -0600

    Update Example_22.cpp

--- Inheritance/Example_22.cpp
+++ Inheritance/Example_22.cpp
@@ -12,33 +12,33 @@ The implicit copying constructor makes a twin copy of an object. Note that this 
 This means that the data field will obviously be copied (cloned) into the newly created object, but in effect it will point to the same piece of memory.
 This also means that different objects may have something in common – they may share some data among them.*/
 class Class {
-	int *data;
+    int *data;
 public:
-	Class(int value) 
-	{
-		 data = new int;
-		*data = value;
-	}
-	void increment()
-	{ 
-		(*data)++; 
-	}
-	int value()
-	{ 
-		return *data; 
-	}
+    Class(int value) 
+    {
+            data = new int;
+        *data = value;
+    }
+    void increment()
+    { 
+        (*data)++; 
+    }
+    int value()
+    { 
+        return *data; 
+    }
 };
 
 int main()
 {
-	Class o1(123);
-	Class o2 = o1;
-	Class o3(o2);
+    Class o1(123);
+    Class o2 = o1;
+    Class o3(o2);
 
-	o1.increment();
-	cout << "o1.value() = " << o1.value() << endl;
-	cout << "o2.value() = " << o2.value() << endl;
-	cout << "o3.value() = " << o3.value() << endl;
+    o1.increment();
+    cout << "o1.value() = " << o1.value() << endl;
+    cout << "o2.value() = " << o2.value() << endl;
+    cout << "o3.value() = " << o3.value() << endl;
 
     askOS();
     return 0; 
