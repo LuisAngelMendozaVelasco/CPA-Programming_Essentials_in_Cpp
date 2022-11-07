7abad5988e40c39485909fe23e203d986afb61a0
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-11-07 12:30:43 -0600

    Create Example_27.cpp

---
+++ Inheritance/Example_27.cpp
@@ -0,0 +1,30 @@
+/* The code shows a class that doesn’t have a constructor at all. We are aware of all the possible hazards connected with the use of uninitialized objects.
+The class will be implicitly equipped with the so-called implicit default (parameter-less) constructor but the constructor will do nothing at all. 
+Our will will be wilful indeed!*/
+#include <iostream>
+#include "../myFunctions.h"
+
+using namespace std;
+
+// The class has no constructor. In effect its fields will not be initialized in any way. The values outputted by the display() method may be completely random then.
+class NoConstructorsAtAll {
+public:
+    int   i;
+    float f;
+    void display()
+    { 
+        cout << "i = " << i << ", f = " << f << endl; 
+    }
+};
+
+int main()
+{
+    NoConstructorsAtAll  o1;
+    NoConstructorsAtAll *o2 = new NoConstructorsAtAll;
+
+    o1.display();
+    o2 -> display();
+
+    askOS();
+    return 0;   
+}
