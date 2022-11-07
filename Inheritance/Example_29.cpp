f20d63b999526f8c1b8f12909dc09ef73989f49c
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-11-07 13:21:55 -0600

    Create Example_29.cpp

---
+++ Inheritance/Example_29.cpp
@@ -0,0 +1,33 @@
+/* We’ve changed the header of the existing constructor by adding default values to both parameters. This means that from a compiler’s perspective, 
+an invocation like this one:
+    WithConstructor()
+is solvable even when the parameter-less constructor cannot be found within the class.
+The compiler will look for another compatible candidate for the invocation and will find it, assuming that it corresponds to the following invocation:
+    WithConstructor(0, 0.)*/
+#include <iostream>
+#include "../myFunctions.h"
+
+using namespace std;
+
+class WithConstructor {
+public:
+    int i;
+    float f;
+    WithConstructor(int a = 0, float b = 0.) : i(a), f(b) { }
+    void display()
+    { 
+        cout << "i = " << i << ", f = " << f << endl; 
+    }
+};
+
+int main()
+{
+    WithConstructor  o1;
+    WithConstructor *o2 = new WithConstructor();
+
+    o1.display();
+    o2 -> display();
+
+    askOS();
+    return 0; 
+}
