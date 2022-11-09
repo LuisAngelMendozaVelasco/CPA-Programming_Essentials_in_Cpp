5aa57192181dce5b942ca7fb237e80bb7fd3eeec
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-11-09 09:39:56 -0600

    Create Example_15.cpp

---
+++ Exceptions/Example_15.cpp
@@ -0,0 +1,36 @@
+/* Proper handling of the bad_exception exception requires the function to specify bad_exception on its throw list (it looks like a paradox but it’s true), 
+and the unexpected handler function must be defined and set. Failure to meet any of these conditions will result in undesired program behaviour.*/
+#include <iostream>
+#include <exception>
+#include "../myFunctions.h"
+
+using namespace std;
+
+void unexp()
+{
+    cout << "Unexpected exception arrived!" << endl;
+    throw;
+}
+
+void function() throw(int, bad_exception) 
+{
+    throw 3.14; 
+}
+
+int main()
+{
+    set_unexpected(unexp);
+    try {
+        function();
+    } 
+    catch(double f) {
+        cout << "Got double" << endl;	
+    } 
+    catch(bad_exception bad) {
+        cout << "It's so bad..." << endl;
+    }
+    cout << "Done" << endl;
+
+    askOS();
+    return 0;
+}
