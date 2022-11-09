0b3dd733cfdd1854f1c53330e535eea56a3a7b1b
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-11-08 21:29:55 -0600

    Create Example_10.cpp

---
+++ Exceptions/Example_10.cpp
@@ -0,0 +1,41 @@
+/* Newer compilers accepts completely differ syntax for the same purpose. A function which throws an exception (no matter which exactly) announces its intent using the following form:.
+    noexcept(false)
+A statement to the contrary is written as:
+    noexcept(true)
+or simply as:
+    noexcept   */
+#include <iostream>
+#include "../myFunctions.h"
+
+using namespace std;
+
+class Class {
+public:
+    string msg;
+    Class(string txt) : msg(txt) {}
+};
+
+void thrower() noexcept(false)
+{
+    cout << "Thrower" << endl;
+    throw Class("object");
+}
+
+void putter() noexcept(true)
+{
+    cout << "Putter" << endl;
+}
+
+int main()
+{
+    try {
+        putter();
+        thrower();
+    }
+    catch(Class &exc) {
+        cout << "Caught!";
+    }
+
+    askOS();
+    return 0;
+}
