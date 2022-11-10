d5953a00946978130612b64edc76184b7a31c826
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-11-09 19:53:38 -0600

    Create Example_34.cpp

---
+++ Exceptions/Example_34.cpp
@@ -0,0 +1,35 @@
+// We’ll check if the stack can handle too many pushes
+#include "mystack.h"
+#include <iostream>
+#include "../myFunctions.h"
+
+using namespace std;
+
+int main()
+{
+    try {
+        Stack stack(1);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        cout << stack.pop() << endl;
+    }
+    catch(stack_bad_alloc &sba) {
+        cout << "No room for the stack - sorry!" << endl;
+    }
+    catch(stack_size_error &sse) {
+        cout << "Stacks of that size don't exist - sorry!" << endl;
+    }
+    catch(stack_overflow &se) {
+        cout << "Stack is too small for that many pushes - sorry!" << endl;
+    }
+    catch(stack_empty &su) {
+        cout << "Stack is empty - sorry!" << endl;
+    }
+    catch(...) {
+        cout << "I don't know what I'm doing";
+    }
+
+    askOS();
+    return 0;
+}
