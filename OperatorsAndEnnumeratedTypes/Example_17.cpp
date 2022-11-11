e6a81e2f61d88c448f880ea0756264fb522bb73f
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2022-11-11 10:34:54 -0600

    Create Example_17.cpp

---
+++ OperatorsAndEnnumeratedTypes/Example_17.cpp
@@ -0,0 +1,32 @@
+/* Main function
+
+The code shows our Fraction class in action*/
+#include "fraction.h"
+#include <iostream>
+#include "../myFunctions.h"
+
+using namespace std;
+
+int main()
+{
+    Fraction f1(1,2), f2(2,3), f;
+
+    cout << f1 << " -> " << f1.get_double() << endl;
+    cout << f2 << " -> " << f2.get_double() << endl;
+    f = f1 + f2;
+    cout << f1 << " + " << f2 << " = " << f << endl;
+    f = f2 + f2 + f2;
+    cout << f2 << " + " << f2 << " + " << f2 << " = " << f << endl;   
+    f = !f;
+    cout << f2 << " + " << f2 << " + " << f2 << " = " << f << endl;
+    f = f1 * f2;
+    cout << f1 << " * " << f2 << " = " << f << endl;
+    f = f1 / f2;
+    cout << f1 << " / " << f2 << " = " << f << endl;
+    Fraction f3(7,8);
+    f3 += f1;
+    cout << f3 << endl;
+
+    askOS();
+    return 0;
+}
